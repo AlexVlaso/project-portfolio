14ac52ed7b9c01d883424624581fc1b2acdd6dd9
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-04-06 14:49:48 +0300

    add script for skills ratings

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,3 +8,9 @@ hamburger.addEventListener("click", () => {
 closeMark.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+const counters = document.querySelectorAll(".skills__ratings-counter"),
+  lines = document.querySelectorAll(".skills__ratings-line span");
+
+counters.forEach((el, i) => {
+  lines[i].style.width = el.innerHTML;
+});
